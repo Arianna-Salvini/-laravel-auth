6348f335eb09ca409c1d48f03ef948af06cd9809
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-05-22 12:29:47 +0200

    Change paginate from 3 to 7

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -15,7 +15,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        return view('admin.projects.index', ['projects' =>Project::orderByDesc('id')->paginate(3)]);
+        return view('admin.projects.index', ['projects' =>Project::orderByDesc('id')->paginate(7)]);
     }
 
     /**
